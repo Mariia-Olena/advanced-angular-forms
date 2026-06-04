7841e08d64a6e64a3f1de9c1f747fc4015683258
Mariia-Olena Stus <mariia-olena.stus@hys-enterprise.com>
2026-06-04 17:26:12 +0300

    add select

--- projects/forms-playground/src/app/playgrounds/custom-select/custom-select-page/custom-select-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/custom-select/custom-select-page/custom-select-page.component.ts
@@ -1,22 +1,21 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectModule } from 'projects/custom-form-controls/src/public-api';
 
 @Component({
   selector: 'app-custom-select-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SelectModule],
   templateUrl: './custom-select-page.component.html',
-  styleUrls: [
-    '../../common-page.scss',
-    './custom-select-page.component.scss',
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['../../common-page.scss', './custom-select-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomSelectPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelectionChanged(value: string | null) {
+    console.log('Selected value:', value);
   }
-
 }
--- projects/forms-playground/src/main.ts
+++ projects/forms-playground/src/main.ts
@@ -2,17 +2,16 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { routes } from './app/app-routing';
-import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { environment } from './environments/environment';
+import { routes } from './app/app-routing';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(RouterModule.forRoot(routes), HttpClientModule),
-  ],
+  providers: [importProvidersFrom(RouterModule.forRoot(routes), HttpClientModule, BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
