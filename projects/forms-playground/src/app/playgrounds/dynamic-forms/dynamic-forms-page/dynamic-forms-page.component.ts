3a1027f45c1899f6fe314c7bb7d8fdf065c2e4a5
Mariia-Olena Stus <mariia-olena.stus@hys-enterprise.com>
2026-06-23 16:07:56 +0300

    add validation

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,23 +1,61 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
+
+import { DynamicControl, DynamicFormConfig } from '../dynamic-forms.model';
+import { banWords } from '../../reactive-forms/validators/ban-words.validator';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dynamic-forms-page.component.html',
   styleUrls: ['../../common-page.scss', './dynamic-forms-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicFormsPageComponent implements OnInit {
+  form = new FormGroup({});
+
   protected formLoadingTrigger = new Subject<'user' | 'company'>();
-  protected formConfig$!: Observable<object>;
+  protected formConfig$!: Observable<DynamicFormConfig>;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.formConfig$ = this.formLoadingTrigger.pipe(switchMap((config) => this.http.get(`assets/${config}.form.json`)));
+    this.formConfig$ = this.formLoadingTrigger.pipe(
+      switchMap((config) => this.http.get<DynamicFormConfig>(`assets/${config}.form.json`)),
+      tap(({ controls }) => this.buildForm(controls)),
+    );
+  }
+
+  private buildForm(controls: DynamicFormConfig['controls']): void {
+    Object.keys(controls).forEach((key) => {
+      const validators = this.resolveValidators(controls[key]);
+      this.form.addControl(key, new FormControl(controls[key].value, validators));
+    });
+  }
+
+  private resolveValidators({ validators = {} }: DynamicControl): Validators {
+    return (Object.keys(validators) as Array<keyof typeof validators>).map((validatorKey) => {
+      const validatorValue = validators[validatorKey];
+
+      if (validatorKey === 'required') return Validators.required;
+      if (validatorKey === 'email') return Validators.email;
+      if (validatorKey === 'minLength' && typeof validatorValue === 'number') {
+        return Validators.minLength(validatorValue);
+      }
+      if (validatorKey === 'banWords' && Array.isArray(validatorValue)) {
+        return banWords(validatorValue);
+      }
+
+      return Validators.nullValidator;
+    });
+  }
+
+  protected onSubmit() {
+    console.log('Submitted data: ', this.form.value);
+    this.form.reset();
   }
 }
--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms.model.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms.model.ts
@@ -1,17 +1,25 @@
+import { ValidatorFn, Validators } from '@angular/forms';
+
 export interface DynamicOption {
   label: string;
   value: string;
 }
+
+type CustomValidators = { banWords: ValidatorFn };
+type ValidatorKeys = keyof Omit<typeof Validators & CustomValidators, 'prototype' | 'compose' | 'composeAsync'>;
 export interface DynamicControl<T = string> {
   controlType: 'input' | 'select';
   type?: string;
   label: string;
   value: T | null;
   options?: DynamicOption[];
+  validators?: {
+    [key in ValidatorKeys]?: unknown;
+  };
 }
 export interface DynamicFormConfig {
   description: string;
   controls: {
     [key: string]: DynamicControl;
-  }[];
+  };
 }
