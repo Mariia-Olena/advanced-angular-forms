b568b118f8f953bfc7a565f53c82f5f5953eab33
Mariia-Olena Stus <mariia-olena.stus@hys-enterprise.com>
2026-06-23 14:44:48 +0300

    setup json config

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,22 +1,23 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dynamic-forms-page.component.html',
-  styleUrls: [
-    '../../common-page.scss',
-    './dynamic-forms-page.component.scss',
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['../../common-page.scss', './dynamic-forms-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicFormsPageComponent implements OnInit {
+  protected formLoadingTrigger = new Subject<'user' | 'company'>();
+  protected formConfig$!: Observable<object>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.formConfig$ = this.formLoadingTrigger.pipe(switchMap((config) => this.http.get(`assets/${config}.form.json`)));
   }
-
 }
